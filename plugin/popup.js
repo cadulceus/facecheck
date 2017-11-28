a5e5c5b99e5d9394c580d5e86b3b2e45556be956
cadulceus <kcf270@nyu.edu>
2017-11-28 04:22:58 -0500

    fuck you danny

--- plugin/popup.js
+++ plugin/popup.js
@@ -25,85 +25,78 @@ function sendRequestPost(url,data,callback) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var gen_pass = document.getElementById('gen_pass');
-    gen_pass.addEventListener('click', function() {
-        sendRequest('http://localhost:5000/gen_pass', function (response) {
-        //alert('My request returned this: ' + response);
-	var json = JSON.parse(response);
-	var password = json['password'];
+  var gen_pass = document.getElementById('gen_pass');
+  gen_pass.addEventListener('click', function() {
+    sendRequest('http://localhost:5000/gen_pass', function (response) {
+      //alert('My request returned this: ' + response);
+    	var json = JSON.parse(response);
+    	var password = json['password'];
+      document.getElementById("status_msg").innerHTML = "Your Password:"+password;
+    });
+    document.getElementById("status_msg").style.display = "block";
+    document.getElementById("passbox").style.display = "none";
+    document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  });
 
-        document.getElementById("status_msg").innerHTML = "Your Password:"+password;
 
-        });
-        document.getElementById("status_msg").style.display = "block";
-        document.getElementById("passbox").style.display = "none";
-        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  var copy_pass = document.getElementById('copy_pass');
+  copy_pass.addEventListener('click', function() {
+  	sendRequest('http://localhost:5000/copy_pass?service='+window.location.hostname, function (response) {
+      //alert('My request returned this: ' + response);
+      var json = JSON.parse(response);
+      var stat = json['status'];
+
+    	if(stat == "error"){
+        document.getElementById("status_msg").innerHTML = stat;
+    	}else{
+        document.getElementById("status_msg").innerHTML = "Your password is copied to the clipboard :)";
+    	}
+    });
+    document.getElementById("status_msg").style.display = "block";
+    document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  });
+
+
+  var train = document.getElementById('train');
+  train.addEventListener('click', function() {
+    document.getElementById("status_msg").innerHTML = "danny waz her";
+    document.getElementById("status_msg").style.display = "block";
+    document.getElementById("passbox").style.display = "none";
+    document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  });
+
+
+  var link = document.getElementById('wipe_user');
+  link.addEventListener('click', function() {
+      document.getElementById("status_msg").innerHTML = "danny waz her";
+      document.getElementById("status_msg").style.display = "block";
+      document.getElementById("passbox").style.display = "none";
+      document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  });
+
+
+  var edit = document.getElementById('modify_password');
+  var input = document.getElementById('confirm_modify_password');
+  edit.addEventListener('click', function() {
+    input.addEventListener('click', function() {
+	    var password = document.getElementById("password").value;
+      var service = window.location.hostname;
+    	var id = "11111";
+    	var json_data = {"service":service,"password":password,"id":id};	
+    	sendRequestPost('http://localhost:5000/edit_item',json_data,function (response) {
+      	var json = JSON.parse(response);
+      	if (json['status'] == 'error' && json['message'] == null) {
+              	document.getElementById("status_msg").innerHTML ='Something went wrong :(';        
+      	}else{
+              	document.getElementById("status_msg").innerHTML = json['message'];        
+      	}
+	    });
     });
 
-    var copy_pass = document.getElementById('copy_pass');
-
-    copy_pass.addEventListener('click', function() {
-	sendRequest('http://localhost:5000/copy_pass?service='+window.location.hostname, function (response) {
-        //alert('My request returned this: ' + response);
-	var json = JSON.parse(response);
-	var stat = json['status'];
-
-	if(stat == "error"){
-	
-        	document.getElementById("status_msg").innerHTML = stat;
-	}else{
-        	document.getElementById("status_msg").innerHTML = "Your password is copied to the clipboard :)";
-	}
-
-
-        });
-
-        document.getElementById("status_msg").style.display = "block";
-        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
-    });
-
-    var train = document.getElementById('train');
-    train.addEventListener('click', function() {
-        document.getElementById("status_msg").innerHTML = "danny waz her";
-        document.getElementById("status_msg").style.display = "block";
-        document.getElementById("passbox").style.display = "none";
-        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
-    });
-
-    var link = document.getElementById('wipe_user');
-    link.addEventListener('click', function() {
-        document.getElementById("status_msg").innerHTML = "danny waz her";
-        document.getElementById("status_msg").style.display = "block";
-        document.getElementById("passbox").style.display = "none";
-        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
-    });
-
-    var edit = document.getElementById('modify_password');
-    var input = document.getElementById('confirm_modify_password');
-    edit.addEventListener('click', function() {
-    	input.addEventListener('click', function() {
-	var password = document.getElementById("password").value;
-        var service = window.location.hostname;
-	var id = "11111";
-	var json_data = {"service":service,"password":password,"id":id};	
-	sendRequestPost('http://localhost:5000/edit_item',json_data,function (response) {
-	var json = JSON.parse(response);
-	if (json['status'] == 'error' && json['message'] == null) {
-        	document.getElementById("status_msg").innerHTML ='Something went wrong :(';        
-	}else{
-        	document.getElementById("status_msg").innerHTML = json['message'];        
-	}
-
-	});
-
-	});
-
-        document.getElementById("status_msg").innerHTML = "Change your password :)";
-	document.getElementById("status_msg").style.display = "block";
-        document.getElementById("passbox").style.display = "block";
-        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
-    });
-
-		
+    document.getElementById("status_msg").innerHTML = "Change your password :)";
+    document.getElementById("status_msg").style.display = "block";
+    document.getElementById("passbox").style.display = "block";
+    document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+  });
 });
 
