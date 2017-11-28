25e097084e54beff5e93805e011e62f268535e98
cadulceus <kcf270@nyu.edu>
2017-11-28 00:56:55 -0500

    i make the best changes

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,9 +1,38 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('test');
-    // onClick's logic below:
+    var link = document.getElementById('gen_pass');
     link.addEventListener('click', function() {
-        alert();
+        document.getElementById("status_msg").innerHTML = "test123";
+        document.getElementById("status_msg").style.display = "block";
+        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
     });
+
+    var link = document.getElementById('copy_pass');
+    link.addEventListener('click', function() {
+        document.getElementById("status_msg").innerHTML = "test123";
+        document.getElementById("status_msg").style.display = "block";
+        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+    });
+
+    var link = document.getElementById('train');
+    link.addEventListener('click', function() {
+        document.getElementById("status_msg").innerHTML = "test123";
+        document.getElementById("status_msg").style.display = "block";
+        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+    });
+
+    var link = document.getElementById('wipe_user');
+    link.addEventListener('click', function() {
+        document.getElementById("status_msg").innerHTML = "test123";
+        document.getElementById("status_msg").style.display = "block";
+        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+    });
+
+    var link = document.getElementById('modify_password');
+    link.addEventListener('click', function() {
+        document.getElementById("status_msg").innerHTML = "test123";
+        document.getElementById("status_msg").style.display = "block";
+        document.getElementById("passbox").style.display = "block";
+        document.getElementById("status_msg").style.backgroundColor = "#5fba7d";
+    });
+
 });
